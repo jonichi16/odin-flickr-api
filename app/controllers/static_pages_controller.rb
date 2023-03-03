3a71eeab7ea3d0325a65d7330c5c05a40a2c3750
Jhonel Villanueva <vvjhonel@gmail.com>
2023-03-04 00:48:09 +0800

    fix: Remove env keys in controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   require 'flickr'
 
   def index
-    @flickr = Flickr.new ENV["FLICKR_API_KEY"], ENV["FLICKR_SHARED_SECRET"]
+    @flickr = Flickr.new
     if params[:user_id]
       @photos = @flickr.people.getPublicPhotos(user_id: params[:user_id])
     end
